678f5317f7bdd9a36e43e549ad09ebae7cc8cd58
Yarendk <tw.yaren@gmail.com>
2024-12-02 17:13:24 +0300

    ...

---
+++ 06_Arrays/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Temel Dizi Örnekleri,
+            //dizi aynı veri tipindeki çok sayıda veriyi bir arada tutmaya çalışan yapılardr;
+            //2.4.6.8
+            //sarı kırmızı mavi turuncu 
+            //adana hatay bursa ankara
+            //sınıf gibi
+            //Değişken türü[] DiziAdı =new DeğişkenTürü[ElemanSayısı]
+            //04:30
+
+            //string[] colors = new string[4];
+            //colors[0] = "Kırmızı";
+            //colors[1] = "Sarı";
+            //colors[2] = "Beyaz";
+            //colors[3] = "Mavi";
+
+            //Console.WriteLine(colors[2]);
+
+            //string[] cities = new string[5];
+            //cities[0] = "Milano";
+            //cities[1] = "Nudapeşte";
+            //cities[2] = "Lyon";
+            //cities[3] = "Kahire";
+            //cities[4] = "Üsküp";
+
+            //Console.WriteLine(cities[2]);
+
+
+            //int[] numbers = new int[10];
+            //numbers[0] = 50;
+            //numbers[1] = 48;
+            //numbers[2] = 698;
+            //numbers[3] = 24;
+            //numbers[7] = 748;
+            //Console.WriteLine(numbers[2]);
+
+            //string[] cities = { "Prag", "Roma", "Atina", "Ankara", "Bursa" };
+            //Console.WriteLine(cities[2]);
+
+
+
+
+
+
+
+            #endregion
+
+            #region Dizideki Tüm Elemanları Listeleme
+
+            //string[] colors = { "Sarı", "Kırmızı", "Beyaz", "Mavi", "Yeşil", "Turuncu", "Pembe" };
+
+            //for(int i=0;i<colors.Length;i++)
+            //{
+            //    Console.WriteLine(colors[i]);
+            //}
+
+            //int[] numbers = { 4, 85, 96, 75, 125, 635, 488, 522, 7456, 2365, 1120 };
+            //for (int i=0; i<numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 3 == 0)
+            //    {
+            //        Console.WriteLine(numbers[i]);
+            //    }
+            //}
+
+            //char[] symbols = { 'a', 'b', 'c', '*', '/', '-' };
+            //for (int i = 0; i < symbols.Length; i++)
+            //{
+            //    Console.WriteLine(symbols[i]);
+            //}
+
+            //dizideki en büyük elemanı bulan *** 18:00
+
+            //int[] myArray = { 47, 85, 95, 41, 25, 635, 789, 86, 100 };
+            //int maxNumber=myArray[0];
+
+            //for (int i = 0; i < myArray.Length; i++)
+            //{ 
+            //    if(myArray[i] > maxNumber)
+            //    {
+            //        maxNumber = myArray[i];
+            //    }
+
+            //}
+            //Console.WriteLine(maxNumber);
+
+            //string[] persons = { "ali", "ahmet", "ayşe", "buse", "cem", "deniz" };
+            //Console.WriteLine(persons.Length);
+
+            ///küçükten büyüğe
+            //int[] numbers = { 48, 85, 52, 41, 86, 10, 22, 35 };
+            //Array.Sort(numbers);
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+            //ters
+            //int[] numbers = { 48, 85, 52, 41, 86, 10, 22, 35 };
+            //Array.Reverse(numbers);
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+
+
+
+
+            #endregion
+
+            #region Dizi Metotlar
+
+            //indeksof dizi içerisindeki aranan değerin indeks sırasını döndürür
+
+            //string[] customers = { "ali", "buse", "ayşegül", "merve", "çınar", "kaya" };
+            //int index = Array.IndexOf(customers, "merve");
+            //Console.WriteLine(index);
+
+            //int[] numbers = { 45, 85, 96, 63, 74, 10, 25, 22, 36 };
+            //Console.WriteLine("Dizinin En Büyük Elemanı: " + numbers.Max() + " Dizinin En Küçük Elemanı: " + numbers.Min());
+
+
+
+
+
+            #endregion
+
+            #region Kullanıcıdan Değer Alma
+
+            //string[] cities = new string[5];
+            //for (int i = 0; i < cities.Length; i++)
+            //{
+            //    Console.Write($"Lütfen {i + 1}. Şehri Giriniz: ");
+            //    cities[i] = Console.ReadLine();
+
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("---------------------");
+
+            //for (int i=0;i<cities.Length;i++)
+            //{
+            //    Console.WriteLine(cities[i]);
+            //}
+
+            //int[] numbers = { 10, 20, 30, 40, 50 };
+
+            //int sum = 0;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    sum += numbers[i];
+            //}
+            //Console.WriteLine(sum);
+
+            //dizi içi sayıları tek çift ayrı ayrı listeleme
+
+            //int[] numbers = { 21, 42, 33, 54, 55, 66, 897, 748, 39, 220 };
+
+            //Console.WriteLine("Çift Sayılar");
+            //Console.WriteLine();
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 0)
+            //    { 
+            //        Console.WriteLine(numbers[i]);
+            //    }
+            //}
+
+            //Console.WriteLine("--------");
+            //Console.WriteLine("Tek Sayılar");
+            //Console.WriteLine();
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 1)
+            //    {
+            //        Console.WriteLine(numbers[i]);
+            //    }
+            //}
+       
+
+           
+
+
+
+
+
+
+            #endregion
+            
+
+        }
+    }
+}
